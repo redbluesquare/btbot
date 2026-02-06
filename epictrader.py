03302671b676be64dc6df6d527342c1690b68006
redbluesquare <darryl@redbluesquare.co.uk>
2026-02-06 12:53:06 +0000

    Updates to position size and trading hours

--- epictrader.py
+++ epictrader.py
@@ -35,19 +35,19 @@ def traderbt():
     positions = ig_service.fetch_open_positions()
     epics = ['CS.D.USCGC.TODAY.IP','IX.D.DOW.DAILY.IP','IX.D.FTSE.DAILY.IP']
     buffer = [4,8,4]
-    buy_size = ['1', '0.15', '0.5']
+    buy_size = ['0.50', '0.1', '0.25']
     sell_size = ['0.25', '0.05', '0.1']
     trailing_stop = [1,2,2]
     deal_window = [3,4,3]
     trading_hours = [
         {'open':2,'close':8},
-        {'open':6,'close':7},
+        {'open':3,'close':7},
         {'open':7,'close':11},
     ]
     trading_hours2 = [
-        {'open':12,'close':14},
+        {'open':12,'close':16},
         {'open':10,'close':18},
-        {'open':7,'close':11},
+        {'open':13,'close':17},
     ]
     
     for index, epic in enumerate(epics):
